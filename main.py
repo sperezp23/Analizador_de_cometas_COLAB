22c5b8717da5582d8fe513820029d12efbec39d8
sperezp23 <sperezp23@gmail.com>
2024-09-20 10:45:05 -0500

    Update: 'Organización del código en carpetas'

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # %% Funciones  
-from Verificar_conexion import verificar_conexion
-from Conectar_con_API_de_COBS import conectar_con_API_de_COBS
-from Tratamiento_de_datos_cometa import tratamiento_de_datos_cometa
-from Curva_de_luz_cruda import curva_de_luz_cruda
-from Conectar_con_API_de_MPC import conectar_con_API_de_MPC
-from Tratamiento_de_datos_con_efemerides import tratamiento_de_datos_con_efemerides
-from Curva_de_luz_reducida import curva_de_luz_reducida
-from Promedio_movil import promedio_movil
-from Curva_de_luz_externa import curva_de_luz_externa
-from Curva_de_luz_externa_promediada import curva_de_luz_externa_promediada
+from Funciones.Verificar_conexion import verificar_conexion
+from Funciones.Conectar_con_API_de_COBS import conectar_con_API_de_COBS
+from Funciones.Tratamiento_de_datos_cometa import tratamiento_de_datos_cometa
+from Funciones.Curva_de_luz_cruda import curva_de_luz_cruda
+from Funciones.Conectar_con_API_de_MPC import conectar_con_API_de_MPC
+from Funciones.Tratamiento_de_datos_con_efemerides import tratamiento_de_datos_con_efemerides
+from Funciones.Curva_de_luz_reducida import curva_de_luz_reducida
+from Funciones.Promedio_movil import promedio_movil
+from Funciones.Curva_de_luz_externa import curva_de_luz_externa
+from Funciones.Curva_de_luz_externa_promediada import curva_de_luz_externa_promediada
 
 # Verificar la conexión a internet.
 conectado_a_internet = verificar_conexion()
