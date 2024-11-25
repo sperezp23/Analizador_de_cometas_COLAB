4b936ab146bc9eb8b6bf30b1ae5be716903eafa1
sperezp23 <sperezp23@gmail.com>
2024-11-25 10:49:23 -0500

    Update: Comparación de las gráficas_1

--- Funciones/Tratamiento_de_datos_cometa.py
+++ Funciones/Tratamiento_de_datos_cometa.py
@@ -3,7 +3,9 @@ import pandas as pd
 def tratamiento_de_datos_cometa(content):
     cometa_df = pd.DataFrame(content['objects'])
     
-    cometa_df['obs_method_key'] = cometa_df.obs_method.apply(lambda registro: registro['key'])
+    cometa_df['obs_method_key'] = cometa_df.obs_method.apply(
+        lambda registro: registro['key'] if registro is not None and 'key' in registro else 'Dato faltante'
+        )
     cometa_df['obs_date'] = pd.to_datetime(pd.to_datetime(cometa_df.obs_date).dt.date)
     cometa_df['magnitude'] = pd.to_numeric(cometa_df.magnitude)
 
