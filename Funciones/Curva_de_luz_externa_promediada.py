de8461bdf86371822276511146378ba1f5453f4e
Santiago Pérez Puerta <78920906+sperezp23@users.noreply.github.com>
2024-12-09 13:12:15 -0500

    Update Curva_de_luz_externa_promediada.py

--- Funciones/Curva_de_luz_externa_promediada.py
+++ Funciones/Curva_de_luz_externa_promediada.py
@@ -8,7 +8,7 @@ def curva_de_luz_externa_promediada(nombre_cometa, curva_de_luz_externa_df, tipo
     labels = {'obs_date':'Observation Date','promedio_movil':'Averaged reduced magnitude'}
     titulo = f'Averaged external lightcurve of {nombre_cometa}'
 
-    ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
+    # ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
 
     fig = px.scatter(curva_de_luz_externa_df, x='obs_date', y='promedio_movil', template= 'plotly_dark', labels= labels, title= titulo)
     fig.update_traces(marker=dict(color='yellow', size=6, line=dict(width=1, color='DarkSlateGrey')))
