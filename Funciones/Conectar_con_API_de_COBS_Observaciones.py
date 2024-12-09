0ce0109e2dd90971cd1332ab43bd5ac7ac9532bc
Santiago Pérez Puerta <78920906+sperezp23@users.noreply.github.com>
2024-12-09 12:52:55 -0500

    Update Conectar_con_API_de_COBS_Observaciones.py

--- Funciones/Conectar_con_API_de_COBS_Observaciones.py
+++ Funciones/Conectar_con_API_de_COBS_Observaciones.py
@@ -9,7 +9,8 @@ def conectar_con_API_de_COBS_Observaciones(nombre_cometa: str, conectado_a_inter
     Link_cops_API = f'https://cobs.si/api/obs_list.api?des={nombre_cometa}&format=json&from_date=&to_date=&exclude_faint=False&exclude_not_accurate=False'
 
     if conectado_a_internet:
-        print(f'{'-'*40}\n✅ Conectando con la base de datos [COBS Observaciones].')
+        aux = '-'*40
+        print(f'{aux}\n✅ Conectando con la base de datos [COBS Observaciones].')
         response = requests.get(Link_cops_API)
 
         if response.status_code == 200:
