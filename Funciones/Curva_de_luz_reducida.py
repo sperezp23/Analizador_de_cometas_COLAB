64bc213aff72aafe55d43cb2fc5fa5b02bd92ab9
Santiago Pérez Puerta <78920906+sperezp23@users.noreply.github.com>
2024-12-09 13:24:27 -0500

    Update Curva_de_luz_reducida.py

--- Funciones/Curva_de_luz_reducida.py
+++ Funciones/Curva_de_luz_reducida.py
@@ -8,7 +8,7 @@ def curva_de_luz_reducida(nombre_cometa, curva_de_luz_procesada_df, tipo_de_graf
     labels = {'obs_date':'Observation Date','magnitud_reducida':'Apparent total magnitude reduced', 'obs_method_key' : 'Observation Method'}
     titulo = f'Reduced lightcurve of {nombre_cometa}'
 
-    ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
+    # ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
 
     fig = px.scatter(curva_de_luz_procesada_df, x='obs_date', y='magnitud_reducida', color='obs_method_key', template= 'plotly_dark', labels= labels, title= titulo)
     fig.update_yaxes(autorange="reversed")
