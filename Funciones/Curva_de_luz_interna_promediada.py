91bdf4514aa79df895af0c918a850efd3763d516
Santiago Pérez Puerta <78920906+sperezp23@users.noreply.github.com>
2024-12-09 13:06:59 -0500

    Update Curva_de_luz_interna_promediada.py

--- Funciones/Curva_de_luz_interna_promediada.py
+++ Funciones/Curva_de_luz_interna_promediada.py
@@ -8,12 +8,12 @@ def curva_de_luz_interna_promediada(nombre_cometa, curva_de_luz_externa_df, tipo
     labels = {'obs_date':'Observation Date','promedio_movil':'Averaged reduced magnitude'}
     titulo = f'Averaged reduced lightcurve of {nombre_cometa}'
 
-    # ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
+    ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
 
     fig = px.scatter(curva_de_luz_externa_df, x='obs_date', y='promedio_movil', template= 'plotly_dark', labels= labels, title= titulo)
     fig.update_traces(marker=dict(color='red', size=6, line=dict(width=1, color='DarkSlateGrey')))
     fig.update_yaxes(autorange="reversed")
-    fig.write_image(ruta_archivos_graficas)
+    # fig.write_image(ruta_archivos_graficas)
     fig.show()
     
     print('✅ Creada: curva de luz promediada.')
