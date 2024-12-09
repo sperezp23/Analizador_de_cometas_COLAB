76061287b7ff39eec9263b9c0e354e1b8512cd59
Santiago Pérez Puerta <78920906+sperezp23@users.noreply.github.com>
2024-12-09 13:07:18 -0500

    Update Curva_de_luz_interna.py

--- Funciones/Curva_de_luz_interna.py
+++ Funciones/Curva_de_luz_interna.py
@@ -8,11 +8,11 @@ def curva_de_luz_interna(nombre_cometa, curva_de_luz_externa_df, tipo_de_grafica
     labels = {'obs_date':'Observation Date','promedio_movil':'Minimum averaged reduced magnitude', 'obs_method_key' : 'Observation Method'}
     titulo = f'Minimum averaged reduced internal lightcurve of {nombre_cometa}'
 
-    # ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
+    ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
 
     fig = px.scatter(curva_de_luz_externa_df, x='obs_date', y='magnitud_reducida', color='obs_method_key', template= 'plotly_dark', labels= labels, title= titulo)
     fig.update_yaxes(autorange="reversed")
-    fig.write_image(ruta_archivos_graficas)
+    # fig.write_image(ruta_archivos_graficas)
     fig.show()
     
     print('✅ Creada: Minima curva de luz reducida.')
