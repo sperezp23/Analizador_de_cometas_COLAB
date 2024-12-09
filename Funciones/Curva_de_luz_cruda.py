cc082ab2c10a19e7f24d7b8bb925606cd5e0a8ba
Santiago Pérez Puerta <78920906+sperezp23@users.noreply.github.com>
2024-12-09 13:00:22 -0500

    Update Curva_de_luz_cruda.py

--- Funciones/Curva_de_luz_cruda.py
+++ Funciones/Curva_de_luz_cruda.py
@@ -8,12 +8,12 @@ def curva_de_luz_cruda(nombre_cometa,curva_de_luz_cruda_df, tipo_de_grafica):
     labels = {'obs_date':'Observation Date','magnitude':'Apparent total magnitude crude', 'obs_method_key' : 'Observation Method'}
     titulo = f'Crude lightcurve of {nombre_cometa}'
 
-    ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
+    # ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo, tipo_de_grafica)
 
     fig = px.scatter(curva_de_luz_cruda_df, x='obs_date', y='magnitude', color='obs_method_key', template= 'plotly_dark', labels= labels, title= titulo)
     fig.update_yaxes(autorange="reversed")
     fig.write_image(ruta_archivos_graficas)
-    # fig.show()
+    fig.show()
 
     aux = '-'*40
     print(f'{aux}\n✅ Creada: curva de luz cruda.')
