1aacef900acf46ebc2bcf432e03477a9fb1dbbf6
sperezp23 <sperezp23@gmail.com>
2024-12-09 21:58:35 -0500

    Create: Curvas_de_luz_interna_externa.py

--- Funciones/Generar_grafica.py
+++ Funciones/Generar_grafica.py
@@ -5,8 +5,9 @@ from Funciones.Curva_de_luz_interna import curva_de_luz_interna
 from Funciones.Curva_de_luz_interna_promediada import curva_de_luz_interna_promediada
 from Funciones.Curva_de_luz_externa import curva_de_luz_externa
 from Funciones.Curva_de_luz_externa_promediada import curva_de_luz_externa_promediada
+from Funciones.Curvas_de_luz_interna_externa import curvas_de_luz_interna_externa
 
-def generar_grafica(opcion_elegida: str,nombre_cometa: str, base_de_datos: object) -> None:
+def generar_grafica(opcion_elegida: str,nombre_cometa: str, base_de_datos_1: object, base_de_datos_2: object = None) -> None:
     '''
     Genera la grafica especificada. Opciones a elegir:
     [1]: Curva de luz cruda,
@@ -14,22 +15,26 @@ def generar_grafica(opcion_elegida: str,nombre_cometa: str, base_de_datos: objec
     [3]: Curva de luz interna,
     [4]: Curva de luz interna promediada,
     [5]: Curva de luz externa,
-    [6]: Curva de luz externa promediada
+    [6]: Curva de luz externa promediada,
+    [7]: Curvas de luz  Interna y externa.
     '''
     if opcion_elegida == '1':
-        curva_de_luz_cruda(nombre_cometa, base_de_datos)
+        curva_de_luz_cruda(nombre_cometa, base_de_datos_1)
 
     elif opcion_elegida == '2':
-        curva_de_luz_reducida(nombre_cometa, base_de_datos)
+        curva_de_luz_reducida(nombre_cometa, base_de_datos_1)
 
     elif opcion_elegida == '3':
-        curva_de_luz_interna(nombre_cometa, base_de_datos)
+        curva_de_luz_interna(nombre_cometa, base_de_datos_1)
 
     elif opcion_elegida == '4':
-        curva_de_luz_interna_promediada(nombre_cometa, base_de_datos)
+        curva_de_luz_interna_promediada(nombre_cometa, base_de_datos_1)
 
     elif opcion_elegida == '5':
-        curva_de_luz_externa(nombre_cometa, base_de_datos)
+        curva_de_luz_externa(nombre_cometa, base_de_datos_1)
 
     elif opcion_elegida == '6':
-        curva_de_luz_externa_promediada(nombre_cometa, base_de_datos)
+        curva_de_luz_externa_promediada(nombre_cometa, base_de_datos_1)
+
+    elif opcion_elegida == '7':
+        curvas_de_luz_interna_externa(nombre_cometa, base_de_datos_1, base_de_datos_2)
