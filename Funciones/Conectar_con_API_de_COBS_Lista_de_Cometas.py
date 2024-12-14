762b88dd79514a7a783d335d6d3d5254d6e9a492
sperezp23 <sperezp23@gmail.com>
2024-12-13 19:06:28 -0500

    Update

--- Funciones/Conectar_con_API_de_COBS_Lista_de_Cometas.py
+++ Funciones/Conectar_con_API_de_COBS_Lista_de_Cometas.py
@@ -10,7 +10,7 @@ def conectar_con_API_de_COBS_Lista_de_Cometas(conectado_a_internet: bool) -> obj
     Link_cops_API = 'https://cobs.si/api/comet_list.api'
 
     if conectado_a_internet:
-        print(f'✅ Conectando con la base de datos [COBS lista de Cometas].')
+        print(f'⌛ Conectando con la base de datos [COBS lista de Cometas].')
         response = requests.get(Link_cops_API)
 
         if response.status_code == 200:
