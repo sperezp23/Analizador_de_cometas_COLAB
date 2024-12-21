42c91a8b58ceafe4e6d5439564375ada84f2bd8c
sperezp23 <sperezp23@gmail.com>
2024-12-21 16:12:11 -0500

    Update

--- Funciones/Conectar_con_API_de_MPC.py
+++ Funciones/Conectar_con_API_de_MPC.py
@@ -6,10 +6,17 @@ def conectar_con_API_de_MPC(curva_de_luz_cruda_df, nombre_cometa):
     fechas = (fecha_final - fecha_inicial).days + 1 if (fecha_final - fecha_inicial).days + 1 <= 1441 else 1441
 
     print('⌛ Conectando con la base de datos [MPC efemerides].')
-    ephemeris = MPC.get_ephemeris(nombre_cometa, start = str(fecha_inicial), number = fechas) 
+    ephemeris = MPC.get_ephemeris(nombre_cometa, start = str(fecha_inicial), number = fechas)
+
+    # Creación del data frame ephemeris filtrada
+    ephemeris_df = ephemeris.to_pandas()
+    ephemeris_df.columns = ephemeris_df.columns.str.lower().str.replace(' ', '_')
+
+    ephemeris_filtrada_df = ephemeris_df[['date', 'delta','r', 'phase']].copy()
+    ephemeris_filtrada_df = ephemeris_filtrada_df.rename(columns = {'date':'obs_date'})
+
     print('✅ Base de datos actualizada [MPC efemerides].')
-    
-    return ephemeris
+    return ephemeris_filtrada_df
 
 if __name__ == '__main__':
     conectar_con_API_de_MPC()
--- Funciones/Tratamiento_de_datos_con_efemerides.py
+++ Funciones/Tratamiento_de_datos_con_efemerides.py
@@ -1,15 +1,8 @@
 # Librerías
 import numpy as np
 
-def tratamiento_de_datos_con_efemerides(curva_de_luz_cruda_df, ephemeris, perihelio):
+def tratamiento_de_datos_con_efemerides(curva_de_luz_cruda_df, ephemeris_filtrada_df, perihelio):
     
-    # Creación del data frame ephemeris filtrada
-    ephemeris_df = ephemeris.to_pandas()
-    ephemeris_df.columns = ephemeris_df.columns.str.lower().str.replace(' ', '_')
-
-    ephemeris_filtrada_df = ephemeris_df[['date', 'delta','r', 'phase']].copy()
-    ephemeris_filtrada_df = ephemeris_filtrada_df.rename(columns = {'date':'obs_date'})
-
     # Unión de las bases de datos COBS y MPC
     curva_de_luz_procesada_df = curva_de_luz_cruda_df.merge(ephemeris_filtrada_df, on='obs_date')
 
