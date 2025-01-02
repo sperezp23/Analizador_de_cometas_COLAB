31d9e521da478951e2e32491e4ebccc0c69572ac
sperezp23 <sperezp23@gmail.com>
2025-01-02 13:08:41 -0500

    Update

--- Funciones/Promedio_movil_maximo.py
+++ Funciones/Promedio_movil_maximo.py
@@ -3,7 +3,7 @@ def promedio_movil_maximo(curva_de_luz_procesada_df, numero_elementos_grupo = 7)
     curva_de_luz_externa_df = curva_de_luz_externa_df.reset_index()
 
     curva_de_luz_externa_df = curva_de_luz_externa_df.copy()
-    curva_de_luz_externa_df['promedio_movil'] = curva_de_luz_externa_df.magnitud_reducida.rolling(window = numero_elementos_grupo).mean()
+    curva_de_luz_externa_df['promedio_movil'] = curva_de_luz_externa_df.magnitud_reducida.rolling(window = numero_elementos_grupo, center= True).mean()
 
     return curva_de_luz_externa_df
 
--- Funciones/Promedio_movil_minimo.py
+++ Funciones/Promedio_movil_minimo.py
@@ -3,7 +3,7 @@ def promedio_movil_minimo(curva_de_luz_procesada_df, numero_elementos_grupo = 9)
     curva_de_luz_externa_df = curva_de_luz_externa_df.reset_index()
 
     curva_de_luz_externa_df = curva_de_luz_externa_df.copy()
-    curva_de_luz_externa_df['promedio_movil'] = curva_de_luz_externa_df.magnitud_reducida.rolling(window = numero_elementos_grupo).mean()
+    curva_de_luz_externa_df['promedio_movil'] = curva_de_luz_externa_df.magnitud_reducida.rolling(window = numero_elementos_grupo, center= True).mean()
 
     return curva_de_luz_externa_df
 
