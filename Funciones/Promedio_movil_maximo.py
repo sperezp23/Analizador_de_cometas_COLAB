7970054a65a8b1efa87e86cc3f18daa3e55ab0c4
sperezp23 <sperezp23@gmail.com>
2025-01-02 13:18:44 -0500

    Update

--- Funciones/Promedio_movil_maximo.py
+++ Funciones/Promedio_movil_maximo.py
@@ -1,11 +1,11 @@
 def promedio_movil_maximo(curva_de_luz_procesada_df, numero_elementos_grupo = 7):
-    curva_de_luz_externa_df = curva_de_luz_procesada_df.groupby(by = 'obs_date').min()
-    curva_de_luz_externa_df = curva_de_luz_externa_df.reset_index()
+    curva_de_luz_interna_df = curva_de_luz_procesada_df.groupby(by = 'obs_date').min()
+    curva_de_luz_interna_df = curva_de_luz_interna_df.reset_index()
 
-    curva_de_luz_externa_df = curva_de_luz_externa_df.copy()
-    curva_de_luz_externa_df['promedio_movil'] = curva_de_luz_externa_df.magnitud_reducida.rolling(window = numero_elementos_grupo, center= True).mean()
-
-    return curva_de_luz_externa_df
+    curva_de_luz_interna_df = curva_de_luz_interna_df.copy()
+    curva_de_luz_interna_df['promedio_movil'] = curva_de_luz_interna_df.magnitud_reducida.rolling(window = numero_elementos_grupo, center= True).mean()
+    curva_de_luz_interna_df.dropna(inplace= True)
+    return curva_de_luz_interna_df
 
 if __name__ == '__main__':
     promedio_movil_maximo()
--- Funciones/Promedio_movil_minimo.py
+++ Funciones/Promedio_movil_minimo.py
@@ -4,7 +4,7 @@ def promedio_movil_minimo(curva_de_luz_procesada_df, numero_elementos_grupo = 9)
 
     curva_de_luz_externa_df = curva_de_luz_externa_df.copy()
     curva_de_luz_externa_df['promedio_movil'] = curva_de_luz_externa_df.magnitud_reducida.rolling(window = numero_elementos_grupo, center= True).mean()
-
+    curva_de_luz_externa_df.dropna(inplace= True)
     return curva_de_luz_externa_df
 
 if __name__ == '__main__':
