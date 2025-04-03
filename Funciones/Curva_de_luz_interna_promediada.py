522e11e8b1229a9d5c386ad9ce1bfb738e7799f2
Santiago Pérez Puerta <sperezp23@gmail.com>
2025-04-03 00:18:38 -0500

    Update: 'Eliminando más código basura'

--- Funciones/Curva_de_luz_interna_promediada.py
+++ Funciones/Curva_de_luz_interna_promediada.py
@@ -1,23 +1,17 @@
 # Librerías
 import plotly.express as px
 
-# Funciones
-# from Funciones.Crear_carpetas import crear_carpetas
-
-def curva_de_luz_interna_promediada(nombre_cometa, curva_de_luz_externa_df):
+def curva_de_luz_interna_promediada(nombre_cometa, curva_de_luz_interna_df):
     labels = {
         'obs_date':'Observation Date',
         'promedio_movil':'Averaged reduced magnitude'
         }
     
-    titulo = f'Averaged reduced lightcurve of {nombre_cometa}'
+    titulo = f'Averaged internal lightcurve of {nombre_cometa}'
 
-    # ruta_archivos_graficas = crear_carpetas(nombre_cometa, titulo)
-
-    fig = px.scatter(curva_de_luz_externa_df, x='obs_date', y='promedio_movil', template= 'plotly_dark', labels= labels, title= titulo)
+    fig = px.scatter(curva_de_luz_interna_df, x='obs_date', y='promedio_movil', template= 'plotly_dark', labels= labels, title= titulo)
     fig.update_traces(marker=dict(color='red', size=6, line=dict(width=1, color='DarkSlateGrey')))
     fig.update_yaxes(autorange="reversed")
-    # fig.write_image(ruta_archivos_graficas)
     fig.show()
 
 if __name__ == '__main__':
